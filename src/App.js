c5363094d0207ad574d74c89118209f31c31a566
akshayrx <hello.arxports@gmail.com>
2024-06-29 00:09:27 +0530

    title updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React & Next JS Course Series
+          React & Next JS Course Series June 2024
         </p>
         <a
           className="App-link"
